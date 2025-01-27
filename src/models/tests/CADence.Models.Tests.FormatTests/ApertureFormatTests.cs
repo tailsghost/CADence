520a196bee64c4f850d053d3c5a4cc3a8974a431
gregory <burtelgamerpro@gmail.com>
2025-01-27 23:23:10 +0600

    step 0.1

--- src/models/tests/CADence.Models.Tests.FormatTests/ApertureFormatTests.cs
+++ src/models/tests/CADence.Models.Tests.FormatTests/ApertureFormatTests.cs
@@ -1,15 +1,24 @@
-﻿namespace CADence.Models.Tests.FormatTests
+﻿using CADence.Format;
+
+namespace CADence.Models.Tests.FormatTests
 {
     public class Tests
     {
+        private ApertureFormat _apertureFormat;
+
         [SetUp]
         public void Setup()
         {
+            _apertureFormat = new ApertureFormat();
         }
 
         [Test]
         public void Test1()
         {
+            _apertureFormat.
+
+            _apertureFormat.ParseFixed("242126");
+
             Assert.Pass();
         }
     }
