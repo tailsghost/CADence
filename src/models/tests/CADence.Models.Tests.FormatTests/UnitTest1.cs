a866d45fc49812eba69aa803b73694c106daa06f
gregory <burtelgamerpro@gmail.com>
2025-01-27 23:15:24 +0600

    added test project

---
+++ src/models/tests/CADence.Models.Tests.FormatTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace CADence.Models.Tests.FormatTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
