5a9303b6a7b75c9059ba10e6f2a0fb35e5e4a29e
Григорий Алексеев <burtelgamerpro@gmail.com>
2025-02-08 18:28:27 +0600

    update

--- src/infrastructure/CADence.Infrastructure.Command/Commands/Gerber274x/Commands/Gerber/InstallCommand.cs
+++ src/infrastructure/CADence.Infrastructure.Command/Commands/Gerber274x/Commands/Gerber/InstallCommand.cs
@@ -1,5 +1,6 @@
 using CADence.Infrastructure.Parser.Abstractions;
 using CADence.Infrastructure.Parser.Settings;
+using System.Runtime.Intrinsics.X86;
 
 namespace CADence.Infrastructure.Command.Commands.Gerber274x.Commands.Gerber;
 
@@ -15,6 +16,29 @@ public class InstallCommand : CommandBase<GerberParser274xSettings>
     /// <returns>Обновлённые параметры Gerber-парсера.</returns>
     public override GerberParser274xSettings Execute(GerberParser274xSettings settings)
     {
-        return settings;
+        var parameters = new Dictionary<char, double> { { 'X', settings.Pos.X }, { 'Y', settings.Pos.Y }, { 'I', 0 }, { 'J', 0 } };
+
+        var d = -1;
+        var code = ' ';
+        var start = 0;
+
+        for (int i = 0; i <= cmd.Length; i++)
+        {
+            char c = (i < cmd.Length) ? cmd[i] : 'Z';
+            if (i == cmd.Length || char.IsLetter(c))
+            {
+                if (code == 'D')
+                    d = int.Parse(cmd[start..i]);
+                else if (code == 'I' || code == 'J')
+                {
+                    parameters[code] = fmt.ParseFixed(cmd[start..i]);
+                }
+                else if (code != ' ')
+                    parameters[code] = fmt.ParseFixed(cmd[start..i]);
+
+                code = c;
+                start = i + 1;
+            }
+        }
     }
 }
